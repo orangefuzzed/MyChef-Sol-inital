a796bbef58e595659a94412f768dd7916fcb8149
orangefuzzed <makeeverydaygreat@pm.me>
2024-10-31 14:22:54 -0600

    resolve module not found on route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 // Updated app/api/auth/[...nextauth]/route.ts
 
 import NextAuth from 'next-auth';
-import { authOptions } from './authOptions';
+import { authOptions } from '../authOptions';
 
 const handler = NextAuth(authOptions);
 
