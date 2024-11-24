a4e4857de9485104d9b2992f2d7864f33588e9e2
orangefuzzed <makeeverydaygreat@pm.me>
2024-11-23 19:29:31 -0700

    V.10 round 4 fixes: searchparams issue - added loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+// loading.tsx
+'use client';
+
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p className="text-white">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
