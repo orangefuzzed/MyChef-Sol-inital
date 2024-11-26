fc8e973d8f1338895a5b4cced1c22a29486756ce
orangefuzzed <makeeverydaygreat@pm.me>
2024-11-26 16:20:56 -0700

    V.10 _app.tsx, manifest & sw.js test 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../styles/globals.css';
+import '../app/globals.css';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
