2a99aa8807357e054a6b08877e11513743758f7b
orangefuzzed <makeeverydaygreat@pm.me>
2024-12-07 10:47:17 -0700

    V.10 input field change to text-area test 3

--- app/components/AIChatInterface/MessageInput.tsx
+++ app/components/AIChatInterface/MessageInput.tsx
@@ -20,7 +20,7 @@ const MessageInput: React.FC<MessageInputProps> = ({
   return (
     <div className="relative p-2 mb-2">
       {/* Textarea with dynamic resizing */}
-      <div className="flex items-center bg-slate-500/30 backdrop-blur-lg shadow-lg ring-1 ring-black/5 rounded-full border border-slate-400 p-2">
+      <div className="flex items-center bg-slate-500/30 backdrop-blur-lg shadow-lg ring-1 ring-black/5 rounded-3xl border border-slate-400 p-1">
         <textarea
           value={inputMessage}
           onChange={(e) => {
@@ -35,7 +35,7 @@ const MessageInput: React.FC<MessageInputProps> = ({
           disabled={isLoading}
         />
 
-        {/* Navigation Buttons */}
+        {/* Navigation Buttons 
         <div className="flex space-x-2 mr-10">
           <button className="p-1 text-gray-400" disabled={isLoading}>
             <ChevronLeftIcon className="h-5 w-5" />
@@ -43,21 +43,19 @@ const MessageInput: React.FC<MessageInputProps> = ({
           <button className="p-1 text-gray-400" disabled={isLoading}>
             <ChevronRightIcon className="h-5 w-5" />
           </button>
-        </div>
-        {/* Microphone Button */}
-        <button className="text-black ml-2 mr-2">
-          <Mic strokeWidth={1.5} className="h-6 w-6" />
-        </button>
-      </div>
-
-      {/* Send Button Detached */}
-      <div className="absolute right-12 bottom-6 transform translate-y-1/2">
+        </div>*/}
+        
+        {/* Send Button */}
         <button
           onClick={handleSendMessage}
-          className="p-3 rounded-full bg-[#27ff52] text-black border border-slate-400 shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+          className="p-2 rounded-full bg-[#27ff52] text-black border border-slate-400 shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
           disabled={isLoading}
         >
           <PaperPlaneIcon className="h-4 w-4" />
+        </button>
+        {/* Microphone Button */}
+        <button className="text-black ml-2 mr-2">
+          <Mic strokeWidth={1.5} className="h-6 w-6" />
         </button>
       </div>
     </div>
