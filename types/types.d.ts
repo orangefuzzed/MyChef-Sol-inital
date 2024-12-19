44712997e1c685cc4b9fbbec333c2c0693bfd7e0
orangefuzzed <makeeverydaygreat@pm.me>
2024-12-18 17:39:24 -0700

    BETA V.1 - wake lock msg for iOS users test 1...again - type.d.ts update

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,14 +1,14 @@
 // types.d.ts
 interface BeforeInstallPromptEvent extends Event {
-    prompt: () => Promise<void>;
-    userChoice: Promise<{ outcome: 'accepted' | 'dismissed' }>;
-  }
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed' }>;
+}
 
-  declare global {
-    interface Window {
-      opera?: unknown; // Optional property since it's not always present
-    }
+declare global {
+  interface Window {
+    opera?: unknown; // Optional property since it's not always present
+    BeforeInstallPromptEvent?: BeforeInstallPromptEvent; // Extend global Window
   }
-  export {};
-  
-  
+}
+
+export {};
