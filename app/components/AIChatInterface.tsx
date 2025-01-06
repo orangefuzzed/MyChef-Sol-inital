8a262e4e0c832496e1f686d67dd4547de95fad68
“orangefuzzed” <makeeverydaygreat@pm.me>
2025-01-05 19:27:32 -0700

    BETA V.1 - window not defined issue fix - AIChatInterface window not scrollable - fix 1

--- app/components/AIChatInterface.tsx
+++ app/components/AIChatInterface.tsx
@@ -69,21 +69,6 @@ const handlePairingsRequest = async (pairingRequest: string) => {
   }
 };
 
-useEffect(() => {
-  // Disable pull-to-refresh in this specific component
-  const disablePullToRefresh = (e: TouchEvent) => {
-    if (e.touches.length === 1 && e.touches[0].clientY > 0) {
-      e.preventDefault();
-    }
-  };
-
-  document.addEventListener('touchmove', disablePullToRefresh, { passive: false });
-
-  return () => {
-    document.removeEventListener('touchmove', disablePullToRefresh);
-  };
-}, []);
-
   const router = useRouter();
 
   const { startNewSession } = useChat();
